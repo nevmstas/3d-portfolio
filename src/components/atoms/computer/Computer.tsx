82914da9fbf3cc96294da30a2a16354e816b4fe3
nevmstas <nevmyvakastas@gmail.com>
2023-08-02 00:34:51 +0600

    desc & 3d model

---
+++ src/components/atoms/computer/Computer.tsx
@@ -0,0 +1,33 @@
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from "react"
+
+const Computer = () => {
+    const computer = useGLTF('./desktop_pc/scene.gltf')
+    return <mesh>
+        <hemisphereLight intensity={5}
+            groundColor={"black"} />
+        {/* <pointLight intensity={1} />
+        <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} /> */}
+        <primitive object={computer.scene}
+            scale={0.65}
+            position={[0, -2.5, -1.5]}
+            rotation={[0, -0.2, -0.1]}
+        />
+    </mesh>
+}
+
+const ComputerCanvas = () => {
+    return <Canvas
+        frameloop="demand"
+        shadows
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}>
+        {/* <Suspense fallback={<div>Loader</div>} > */}
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        <Computer />
+        {/* </Suspense> */}
+        <Preload all />
+    </Canvas>
+}
+export default ComputerCanvas
