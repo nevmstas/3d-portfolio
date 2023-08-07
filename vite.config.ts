b8cc816610b014772bc5d938fee5830152c2dba5
nevmstas <nevmyvakastas@gmail.com>
2023-08-07 12:17:21 +0600

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [react()],
+  base: './index.html',
 })
